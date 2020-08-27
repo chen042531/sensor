93f3317067270c5fa3902fa7f0edee72afd76cc4
ycchen <chen042531.cs03@nctu.edu.tw>
2020-08-27 09:56:03 +0800

    observer success UI update, textView -> View

--- sensing/src/main/java/com/example/sensing/DListener.java
+++ sensing/src/main/java/com/example/sensing/DListener.java
@@ -2,15 +2,16 @@ package com.example.sensing;
 
 import android.hardware.SensorEventListener;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class DListener implements DataListener{
     sensor observerable;
-    TextView tv;
+    View tv;
     public DListener(sensor observerable){
         this.observerable = observerable;
     }
-    public DListener(sensor observerable, TextView tv1){
+    public DListener(sensor observerable, View tv1){
         this.observerable = observerable;
         this.tv = tv1;
     }
